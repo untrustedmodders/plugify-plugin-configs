0d0e8ede191ae8068135029558216b4bc1839f92
Maxim Telezhenko <zlo911111@gmail.com>
2024-12-21 21:07:01 +0300

    Rename exports macro

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,9 +1,9 @@
 #include "plugin.hpp"
-#include <plugin_export.h>
+#include <plugify_configs_export.h>
 
 void ConfgisPlugin::OnPluginStart()
 {
 }
 
 ConfgisPlugin g_configsPlugin;
-EXPOSE_PLUGIN(PLUGIN_API, &g_configsPlugin)
+EXPOSE_PLUGIN(PLUGIFY_CONFIGS_API, &g_configsPlugin)
