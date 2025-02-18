7b3ccc105a806b24c6199abb4c9c1b307f2bbaa1
Maxim Telezhenko <zlo911111@gmail.com>
2025-02-19 02:22:14 +0300

    Json reader

--- src/readers/json_glaze.cpp
+++ src/readers/json_glaze.cpp
@@ -6,6 +6,44 @@
 
 namespace pcf
 {
+	static void MapJson(glz::json_t& json, Config& config)
+	{
+		if (json.is_null()) {
+			config.Set(nullptr);
+		}
+		else if (json.is_boolean()) {
+			config.Set(json.get_boolean());
+		}
+		else if (json.is_number()) {
+			config.Set(json.get_number());
+		}
+		else if (json.is_string()) {
+			config.Set(json.get_string());
+		}
+		else if (json.is_object()) {
+			config.Set(cfobject);
+			auto& obj = json.get_object();
+			for (auto& field : obj) {
+				config.JumpKey(std::get<0>(field), true);
+				MapJson(std::get<1>(field), config);
+				config.JumpBack();
+			}
+		}
+		else if (json.is_array()) {
+			config.Set(cfarray);
+			auto& arr = json.get_array();
+			for (auto& item : arr) {
+				config.PushNull();
+				config.JumpLast();
+				MapJson(item, config);
+				config.JumpBack();
+			}
+		}
+		else {
+			config.Set(nullptr);
+		}
+	}
+
 	std::unique_ptr<Config> ReadJsonConfig(std::string_view path)
 	{
 		glz::json_t json{};
@@ -15,7 +53,7 @@ namespace pcf
 			return nullptr;
 		}
 		auto config = MakeConfig();
-		config->Set(444);
+		MapJson(json, *config);
 		return config;
 	}
 }
