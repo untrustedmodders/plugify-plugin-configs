928a797f3ccd254d4aa79b4415bf4bfc70d8fc02
Nikita Ushakov <nikita.ushakov0000@outlook.com>
2025-04-01 12:33:41 +0100

    fix: methods exports on Linux

--- include/plugify-configs/methods.hpp
+++ include/plugify-configs/methods.hpp
@@ -8,6 +8,7 @@ namespace pcf
 {
 	class Config;
 
+
 	PLUGIFY_CONFIGS_API std::unique_ptr<pcf::Config> ReadConfig(std::string_view path);
 	PLUGIFY_CONFIGS_API std::unique_ptr<pcf::Config> ReadConfigs(std::span<const std::string_view> paths);
 	PLUGIFY_CONFIGS_API std::unique_ptr<pcf::Config> MakeConfig();
--- src/methods.cpp
+++ src/methods.cpp
@@ -3,26 +3,29 @@
 #include <memory>
 #include <string_view>
 
+#include <plugify-configs/plugify-configs.hpp>
+#include <plugify_configs_export.h>
+
 namespace pcf {
 	extern ConfigsPlugin plugin;
-}
 
-std::unique_ptr<pcf::Config> ReadConfig(std::string_view path) {
-	return pcf::plugin.ReadConfig(path);
-}
+	std::unique_ptr<pcf::Config> ReadConfig(std::string_view path) {
+		return pcf::plugin.ReadConfig(path);
+	}
 
-std::unique_ptr<pcf::Config> ReadConfigs(std::span<const std::string_view> paths) {
-	return pcf::plugin.ReadConfigs(paths);
-}
+	std::unique_ptr<pcf::Config> ReadConfigs(std::span<const std::string_view> paths) {
+		return pcf::plugin.ReadConfigs(paths);
+	}
 
-std::unique_ptr<pcf::Config> MakeConfig() {
-	return pcf::Config::Detail::MakeConfig();
-}
+	std::unique_ptr<pcf::Config> MakeConfig() {
+		return pcf::Config::Detail::MakeConfig();
+	}
 
-void SetError(std::string_view error) {
-	pcf::plugin.SetError(error);
-}
+	void SetError(std::string_view error) {
+		pcf::plugin.SetError(error);
+	}
 
-std::string_view GetError() {
-	return pcf::plugin.GetError();
+	std::string_view GetError() {
+		return pcf::plugin.GetError();
+	}
 }
