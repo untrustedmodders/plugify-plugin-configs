8a8e8a026d95b2873686ad1205daa574c093fd80
Nikita Ushakov <nikita.ushakov0000@outlook.com>
2025-04-01 12:23:47 +0100

    fix: add missing namespace

--- include/plugify-configs/methods.hpp
+++ include/plugify-configs/methods.hpp
@@ -7,10 +7,11 @@
 namespace pcf
 {
 	class Config;
+
+	PLUGIFY_CONFIGS_API std::unique_ptr<pcf::Config> ReadConfig(std::string_view path);
+	PLUGIFY_CONFIGS_API std::unique_ptr<pcf::Config> ReadConfigs(std::span<const std::string_view> paths);
+	PLUGIFY_CONFIGS_API std::unique_ptr<pcf::Config> MakeConfig();
+	PLUGIFY_CONFIGS_API void SetError(std::string_view error);
+	PLUGIFY_CONFIGS_API std::string_view GetError();
 }
 
-PLUGIFY_CONFIGS_API std::unique_ptr<pcf::Config> ReadConfig(std::string_view path);
-PLUGIFY_CONFIGS_API std::unique_ptr<pcf::Config> ReadConfigs(std::span<const std::string_view> paths);
-PLUGIFY_CONFIGS_API std::unique_ptr<pcf::Config> MakeConfig();
-PLUGIFY_CONFIGS_API void SetError(std::string_view error);
-PLUGIFY_CONFIGS_API std::string_view GetError();
