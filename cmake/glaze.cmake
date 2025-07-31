cd3c4242cd597663b60c5b7413c29104fe2b6835
Nikita Ushakov <ushakovn@tcd.ie>
2025-07-31 16:41:19 +0100

    fix: add C++23 support

--- cmake/glaze.cmake
+++ cmake/glaze.cmake
@@ -2,10 +2,16 @@ include(FetchContent)
 
 message(STATUS "Pulling and configuring glaze")
 
+if (CMAKE_CXX_STANDARD GREATER 20)
+    set(GLAZE_TAG "v5.5.4" CACHE STRING "")
+else()
+    set(GLAZE_TAG "v2.9.5" CACHE STRING "")
+endif()
+
 FetchContent_Declare(
         glaze
         GIT_REPOSITORY https://github.com/stephenberry/glaze.git
-        GIT_TAG v2.9.5
+        GIT_TAG ${GLAZE_TAG}
 )
 
 FetchContent_MakeAvailable(glaze)
